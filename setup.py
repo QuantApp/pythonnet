9fe7c9a3d576fff855d5ef24bea63421d17290b2
QuantApp <arturo.rodriguez@quantapp.net>
2017-07-05 22:02:26 +0000

    v3

--- setup.py
+++ setup.py
@@ -371,7 +371,7 @@ if not os.path.exists(_get_interop_filename()):
 
 setup(
     name="pythonnet",
-    version="2.4.0.dev3",
+    version="2.4.0.dev4",
     description=".Net and Mono integration for Python",
     url='https://pythonnet.github.io/',
     license='MIT',
